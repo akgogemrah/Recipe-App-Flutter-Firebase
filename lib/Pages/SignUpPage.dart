ec83959c38b840e5568e674d92ab8dd1fae77ae6
akgogemrah <emrahakgog24@gmail.com>
2023-04-17 18:08:23 +0300

    firebaseauth_added

--- lib/Pages/SignUpPage.dart
+++ lib/Pages/SignUpPage.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
 import 'SignInPage.dart';
-
-
 class SignUpPage extends StatefulWidget {
   const SignUpPage({Key? key}) : super(key: key);
 
